942123ee1452e2736e04ec49364e69470e11dd67
tsalsgiver1 <tsalsgiver1@27a8c586-a725-11de-be8d-735280eae207>
2009-12-05 21:52:44 +0000

    [+]Added some player commands (Still Buggy) [+]Added /goto

--- src/net/sf/odinms/client/messages/commands/WarpCommands.java
+++ src/net/sf/odinms/client/messages/commands/WarpCommands.java
@@ -24,6 +24,7 @@ package net.sf.odinms.client.messages.commands;
 import java.net.InetAddress;
 import java.rmi.RemoteException;
 
+import java.util.HashMap;
 import net.sf.odinms.client.MapleCharacter;
 import net.sf.odinms.client.MapleClient;
 import net.sf.odinms.client.messages.Command;
@@ -40,6 +41,9 @@ import net.sf.odinms.server.maps.MapleMap;
 import net.sf.odinms.tools.MaplePacketCreator;
 
 public class WarpCommands implements Command {
+    MapleClient c;
+    MapleCharacter player = c.getPlayer();
+    ChannelServer cserv = c.getChannelServer();
 	@Override
 	public void execute(MapleClient c, MessageCallback mc, String[] splitted) throws Exception,
 																				IllegalCommandSyntaxException {
@@ -151,7 +155,38 @@ public class WarpCommands implements Command {
 				targetPortal = target.getPortal(0);
 			}
 			c.getPlayer().changeMap(target, targetPortal);
-		} else if (splitted[0].equals("/exprate")) { // by Redline/2azn4u
+
+                } else if (splitted[0].equals("/goto")) {
+                    HashMap<String, Integer> maps = new HashMap<String, Integer>();
+                    maps.put("henesys", 100000000);
+                    maps.put("ellinia", 101000000);
+                    maps.put("perion", 102000000);
+                    maps.put("kerning", 103000000);
+                    maps.put("lith", 104000000);
+                    maps.put("sleepywood", 105040300);
+                    maps.put("florina", 110000000);
+                    maps.put("orbis", 200000000);
+                    maps.put("happy", 209000000);
+                    maps.put("elnath", 211000000);
+                    maps.put("ludi", 220000000);
+                    maps.put("omega", 221000000);
+                    maps.put("korean", 222000000);
+                    maps.put("aqua", 230000000);
+                    maps.put("leafre", 240000000);
+                    maps.put("mulung", 250000000);
+                    maps.put("herb", 251000000);
+                    maps.put("nlc", 600000000);
+                    maps.put("shrine", 800000000);
+                    maps.put("showa", 801000000);
+                    maps.put("fm", 910000000);
+                    if (maps.containsKey(splitted[1])) {
+                        player.changeMap(cserv.getMapFactory().getMap(maps.get(splitted[1])), cserv.getMapFactory().getMap(maps.get(splitted[1])).getPortal(0));
+                    }else{
+                        mc.dropMessage("No map enetered do @goto  <henesys|ellinia|perion|kerning|lith|sleepywood|florina|orbis|happy|elnath|ludi|omega|korean|aqua|leafre|mulung|herb|nlc|shrine|showa|fm>");
+                    }
+                }
+
+		 else if (splitted[0].equals("/exprate")) { // by Redline/2azn4u
 			if (splitted.length > 1) {
 				int exp = Integer.parseInt(splitted[1]);
 				cserv.setExpRate(exp);
@@ -189,6 +224,7 @@ public class WarpCommands implements Command {
 			new CommandDefinition("/lolcastle", "[1-5]", "Warps you into Field of Judgement with the given level", 1),
 			new CommandDefinition("/jail", "[2] playername", "Warps the player to a map that he can't leave", 1),
 			new CommandDefinition("/map", "mapid", "Warps you to the given mapid (use /m instead)", 1),
+                        new CommandDefinition("/goto", "", "Warps you to chosen locations", 1),
 			new CommandDefinition("/exprate", "rate", "Changes the exp rate", 4),
 			new CommandDefinition("/droprate", "rate", "Changes the drop rate", 4),
 			new CommandDefinition("/bossdroprate", "rate", "Changes the boss drop rate", 4),
