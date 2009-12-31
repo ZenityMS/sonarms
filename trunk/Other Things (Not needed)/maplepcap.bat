c99f5647f872ba64762810315728d5f9f09847cc
tsalsgiver1 <tsalsgiver1@27a8c586-a725-11de-be8d-735280eae207>
2009-12-31 19:49:50 +0000

    Changing repacks.

--- trunk/Other Things (Not needed)/maplepcap.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set CLASSPATH=.;dist\*
-java -Dnet.sf.odinms.recvops=recvops.properties -Dnet.sf.odinms.sendops=sendops.properties net.sf.odinms.exttools.maplepcap.MaplePcap mob_skill_status.pcap > mob_skill_status.txt
-pause
--- trunk/Other Things (Not needed)/wzextract.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set CLASSPATH=.;dist\*
-java net.sf.odinms.exttools.wzextract.XMLWzExtract wz\Map.wz
-pause
